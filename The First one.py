0533dbd9ac0931bf3a1aca908258c2976ab875b9
Owner <darron_mitchell@hotmail.com>
2025-07-30 14:51:29 +1200

    Initial Commit

---
+++ The First one.py
@@ -0,0 +1,8 @@
+print("hello")
+import openpyxl as oxl
+import pandas as pd
+data = pd.read_excel(r"c:\Users\Owner\.cache\kagglehub\datasets\nphantawee\pump-sensor-data\versions\1\sensor.xlsx")
+
+
+
+
